11cd0aeb605d39aaf45071dd90df53d9c6bd1df8
martiniazzz <32032050+martiniazzz@users.noreply.github.com>
2019-10-09 19:49:21 +0300

    Added bindings for Fonts & Font Size ComboBoxes

---
+++ TextEditor/TextEditor/FontList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Media;
+
+namespace KMA.APRZP2019.TextEditorProject.TextEditor
+{
+    class FontList : ObservableCollection<string>
+    {
+        public FontList()
+        {
+            foreach (FontFamily f in Fonts.SystemFontFamilies)
+            {
+                this.Add(f.ToString());
+            }
+        }
+    }
+}
