45f8136a748edf297e7304ca36d0e5d3a5fba02b
martiniazzz <32032050+martiniazzz@users.noreply.github.com>
2019-10-09 22:29:57 +0300

    File open, save & new options added

---
+++ TextEditor/TextEditor/FileOptionHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace KMA.APRZP2019.TextEditorProject.TextEditor
+{
+    class FileOptionHandler
+    {
+
+        
+
+    }
+}
--- TextEditor/TextEditor/MainWindow.xaml.cs
+++ TextEditor/TextEditor/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
 using System.Windows.Documents;
+using System.Windows.Input;
 
 namespace TextEditor
 {
@@ -8,9 +11,62 @@ namespace TextEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private bool _isSaved = false;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        public bool IsSaved
+        {
+            get
+            {
+                return _isSaved;
+            }
+            set
+            {
+                _isSaved = value;
+            }
+        }
+
+        private void New_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!_isSaved)
+            {
+                //SaveChangesDialog();
+                Save_Executed(sender, e);
+            }
+            MainTextBox.Document.Blocks.Clear();
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+                TextRange range = new TextRange(MainTextBox.Document.ContentStart, MainTextBox.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
+                fileStream.Close();
+            }
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                using (FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create))
+                {
+                    TextRange range = new TextRange(MainTextBox.Document.ContentStart, MainTextBox.Document.ContentEnd);
+                    range.Save(fileStream, DataFormats.Rtf);
+                }
+            }
+        }
+
     }
 }
