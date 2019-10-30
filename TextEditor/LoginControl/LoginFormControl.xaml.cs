5d1c3ff39f67900cecf5507ccf84aa50c041ac0b
martiniazzz <32032050+martiniazzz@users.noreply.github.com>
2019-10-30 16:16:44 +0200

    LoginForm in separate project created

---
+++ TextEditor/LoginControl/LoginFormControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using KMA.APRZP2019.TextEditorProject.TextEditor;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LoginControl
+{
+    /// <summary>
+    /// Interaction logic for LoginFormControl.xaml
+    /// </summary>
+    public partial class LoginFormControl : UserControl
+    {
+        public LoginFormControl()
+        {
+            InitializeComponent();
+            //DataContext = new LogInViewModel();
+        }
+
+        private void LogIn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(Username.Text) || String.IsNullOrWhiteSpace(Password.Password))
+            {
+                MessageBox.Show("Login or password is empty!");
+                return;
+            };
+            MessageBox.Show($"Login successful for user {Username.Text}");
+            
+        }
+
+        private void Register_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(Username.Text) || String.IsNullOrWhiteSpace(Password.Password))
+            {
+                MessageBox.Show("Login or password is empty!");
+                return;
+            }
+            MessageBox.Show($"User with name {Username.Text} was created");
+            // todo switch to main window
+        }
+    }
+}
